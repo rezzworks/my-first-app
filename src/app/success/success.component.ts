c66212e230f821ee23164f28ccc2576bb01b4669
rezzworks <john.beasley.webdev@gmail.com>
2018-11-07 23:45:47 -0500

    found error

---
+++ src/app/success/success.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-success',
+	templateUrl: './success.component.html'
+    styles: [`
+	h3 {
+		color: green;
+	}
+  `]	
+})
+
+export class SuccessComponent {
+	
+}
--- src/app/warning/warning.component.ts
+++ src/app/warning/warning.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
 	selector: 'app-warning',
 	templateUrl: './warning.component.html'
-});
+    styles: [`
+	h3 {
+		color: red;
+	}
+  `]	
+})
 
 export class WarningComponent {
 	
