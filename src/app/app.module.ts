f1969e94005449a8921aa79f7fbce86f6b7c2174
rezzworks <john.beasley.webdev@gmail.com>
2018-11-08 23:23:26 -0500

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningComponent } from './warning/warning.component';
 import { SuccessComponent } from './success/success.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { SuccessComponent } from './success/success.component';
 	ServerComponent,
 	ServersComponent,
 	WarningComponent,
-	SuccessComponent
+	SuccessComponent,
+	ErrorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/error/error.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'app-error',
+	templateUrl: './error.component.html'
+    styles: [`
+	h3 {
+		color: red;
+	}
+  `]	
+})
+
+export class ErrorComponent {
+	
+}
