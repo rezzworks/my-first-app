1e7f6709bec6128740c1c6cb1c4a59711bc56b50
rezzworks <john.beasley.webdev@gmail.com>
2019-03-11 22:31:37 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css']/*,
   styles: [`
 	h3 {
 		color: dodgerblue;
-	}
-  `]
+  }
+  .white-text {
+    color: white;
+  }
+  `]*/
 })
 export class AppComponent {
   username1 = '';
+  showSecret = false;
+  log = [];
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
